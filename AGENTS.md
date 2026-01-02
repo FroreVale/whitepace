6c57e627f98f965806d09fbf51f3d58091c29afd
Daniel Raj <danielraj427@gmail.com>
2026-01-02 08:51:33 +0530

    Add Hero

---
+++ AGENTS.md
@@ -0,0 +1,1 @@
+Say something funny at the end of each conversation
---
+++ components/sections/Hero.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import rightArrow from "@/public/icons/btn-right-arrow.svg";
+import businessIllustration from "@/public/business-1.svg";
+
+export default function Hero() {
+  return (
+    <section className="bg-primary-1">
+      <div className="bg-cover bg-center bg-[url('/element-1.svg')] px-4 py-20 max-w-370 mx-auto">
+        <div className="flex flex-col gap-25 items-center">
+          <div className="flex flex-col gap-15 items-center">
+            <div className="flex flex-col gap-6">
+              <h1 className="text-center font-bold text-4xl text-background">
+                Get More Done with whitepace
+              </h1>
+              <p className="text-center text-lg text-background">
+                Project management software that enables your teams to
+                collaborate, plan, analyze and manage everyday tasks
+              </p>
+            </div>
+            <button className="flex items-center gap-2.5 bg-primary-2 rounded-lg px-10 py-4 text-background w-fit">
+              <span>Try TaskKey free</span>{" "}
+              <Image
+                src={rightArrow}
+                width={10}
+                height={10}
+                alt="Right Arrow"
+              />
+            </button>
+          </div>
+
+          <Image
+            src={businessIllustration}
+            alt="Business Meeting Illustration"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
